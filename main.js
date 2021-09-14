c98bcf28fb0c2bf6d2415af3c66eb7cd6bcdd99f
Najikethas <86484325+Najikethas@users.noreply.github.com>
2021-09-14 21:42:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("My  Project On Console")
+var First_name="Najikethas"
+var Last_name="Renjith"
+var Full_name=First_name.concat(Last_name)
+console.log(Full_name)
+var a=10;
+var b=5;
+var add=a+b;
+console.log(add);
+var sub=a-b;
+console.log(sub);
+var mul=a*b;
+console.log(mul); 
